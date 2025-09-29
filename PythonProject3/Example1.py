05c498997033818111081e3129fbd297e81c74e4
Enriquesavatia <enriquetruekid@gmail.com>
2025-09-29 17:20:23 +0300

    Remove unuseful file

--- PythonProject3/Example1.py
+++
@@ -1,8 +0,0 @@
-import sys
-def main():
-    name=input("What is your name? ")
-    age=int(input("How old are you? "))
-    print(f"Hello, {name}! You are {age} years old.")
-    return 0
-if __name__ == "__main__":
-    sys.exit(main())
